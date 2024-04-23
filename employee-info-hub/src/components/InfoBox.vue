62a9226b685519e8472388acc24dde022ee3461f
ms.chandralekhapodili <ms.chandralekhapodili@gmail.com>
2024-04-23 16:19:55 -0400

    first commit

---
+++ employee-info-hub/src/components/InfoBox.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="flex-container">
+        <h2>Employee Name </h2>
+        <h2>Employee ID </h2>
+        <h2>Email </h2>
+        <img id="logo" src="https://img.icons8.com/?size=160&id=113856&format=png" alt="Company Logo"/>
+    </div>
+    <div v-for="employee of employeeDetails" :key="employee.id" >
+       <SingleEmployee :employee=employee />
+    </div>
+</template>
+<script>
+import SingleEmployee from './SingleEmployee.vue'
+export default {
+    name: 'InfoBox',
+    props:{
+        employeeDetails: Array
+    },
+    components:{
+        SingleEmployee
+    } 
+}
+</script>
+<style scoped>
+    .flex-container{
+        display:flex;
+        justify-content:space-between;
+        align-items:center;
+        font-family: "Lora", serif;
+        font-weight:500;
+        margin:20px;
+        padding:5px;
+    }
+    #logo{
+        width:80px;
+        height:80px;
+    }
+</style>
---
+++ employee-info-hub/src/components/SingleEmployee.vue
@@ -0,0 +1,59 @@
+<template>
+  <div  id="singleEmployeediv">
+    <p>{{ employee.employeeName }}</p>
+    <p>{{ employee.employeeID }}</p>
+    <p>{{ employee.email }}</p>
+    <button @click="showDetails" class="details-btn">Show Details</button> 
+    <EmployeeDetailsModal :employee=employee v-if="showModal" @close="closeModal" />
+  </div>
+</template>
+<script>
+import EmployeeDetailsModal from './EmployeeDetailsModal.vue';
+export default {
+  name: 'SingleEmployee',
+  props: {
+    employee: Object
+  },
+  components: {
+    EmployeeDetailsModal
+  },
+  data() {
+    return {
+      showModal: false
+    };
+  },
+  methods: {
+    showDetails() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    }
+  }
+};
+</script>
+<style scoped>
+#singleEmployeediv{
+    border:transparent;
+    border-radius:4px;
+    box-shadow:0 4px 8px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+    color: black;
+    padding: 20px;
+    margin: 10px;
+    text-align: center;
+    font-family: "Lora", serif;
+    font-weight:500;
+    display:flex;
+    justify-content:space-between;
+    align-items:center;
+}
+.details-btn{
+  border: transparent;
+  border-radius:5px;
+  padding:10px;
+  cursor:pointer;
+  background-color:#72A0C1;
+  opacity:0.8;
+  color:white;
+}
+</style>
